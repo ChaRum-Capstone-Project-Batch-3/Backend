959a3376b433d6c940190b389daad64b3acc9930
Muhammad Razan Fawwaz <razanfawwaz1905@gmail.com>
2022-11-26 22:02:00 +0700

    feat: add request validate

--- controller/topics/request/json.go
+++ controller/topics/request/json.go
@@ -10,8 +10,8 @@ import (
 )
 
 type Create struct {
-	Topic       string `json:"topic"`
-	Description string `json:"description"`
+	Topic       string `json:"topic" validate:"required"`
+	Description string `json:"description" validate:"required"`
 }
 
 func (req *Create) ToDomain() *topics.Domain {
@@ -19,6 +19,37 @@ func (req *Create) ToDomain() *topics.Domain {
 		Topic:       req.Topic,
 		Description: req.Description,
 	}
+}
+
+func (req *Create) Validate() []helper.ValidationError {
+	var ve validator.ValidationErrors
+
+	if err := validator.New().Struct(req); err != nil {
+		if errors.As(err, &ve) {
+			fields := structs.Fields(req)
+			out := make([]helper.ValidationError, len(ve))
+
+			for i, e := range ve {
+				out[i] = helper.ValidationError{
+					Field:   e.Field(),
+					Message: helper.MessageForTag(e.Tag()),
+				}
+
+				out[i].Message = strings.Replace(out[i].Message, "[PARAM]", e.Param(), 1)
+
+				// Get field tag
+				for _, f := range fields {
+					if f.Name() == e.Field() {
+						out[i].Field = f.Tag("json")
+						break
+					}
+				}
+			}
+			return out
+		}
+	}
+
+	return nil
 }
 
 type Update struct {
@@ -33,7 +64,7 @@ func (req *Update) ToDomain() *topics.Domain {
 	}
 }
 
-func (req *Create) Validate() []helper.ValidationError {
+func (req *Update) Validate() []helper.ValidationError {
 	var ve validator.ValidationErrors
 
 	if err := validator.New().Struct(req); err != nil {
