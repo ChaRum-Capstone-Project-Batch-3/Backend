2d51764a4c477935ba5ba45d44c6bd64dd5a8912
razanfawwaz <razanfawwaz1905@gmail.com>
2022-12-21 21:09:07 +0700

    test: update test for threads

--- business/threads/mocks/UseCase.go
+++ business/threads/mocks/UseCase.go
@@ -177,6 +177,27 @@ func (_m *UseCase) DomainsToResponseArray(domains []threads.Domain, userID primi
 	return r0, r1
 }
 
+// GetAll provides a mock function with given fields:
+func (_m *UseCase) GetAll() (int, error) {
+	ret := _m.Called()
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func() int); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetAllByTopicID provides a mock function with given fields: topicID
 func (_m *UseCase) GetAllByTopicID(topicID primitive.ObjectID) ([]threads.Domain, error) {
 	ret := _m.Called(topicID)
